65dcdfddd23574c3a86127d31edb5f4de3211633
sourcecode121 <sourcecode121@users.noreply.github.com>
2016-10-18 23:03:09 +0530

    Initial commit

---
+++ app/src/main/java/com/example/glide/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.glide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView1;
+    ImageView imageView2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView1 = (ImageView) findViewById(R.id.image_1);
+        imageView2 = (ImageView) findViewById(R.id.image_2);
+
+        Glide.with(this)
+                .load(R.drawable.yoda)
+                .crossFade(5000)
+                .centerCrop()
+                .into(imageView1);
+
+        Glide.with(this)
+                .load(R.drawable.vader)
+                .crossFade(5000)
+                .centerCrop()
+                .into(imageView2);
+    }
+}
